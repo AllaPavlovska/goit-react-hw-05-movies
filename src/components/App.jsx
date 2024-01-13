a8fe2b8724fb142c930512ac99c209e0b1f17b39
AllaPavlovska <pavlovskaa87@gmail.com>
2024-01-13 17:46:13 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { SharedLayout } from "../components/SharedLayout";
 
